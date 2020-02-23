d8b5811210d8803841ad934588a4ac5d1d1a9587
ivanvmoreno <contacto@ivanmoreno.me>
2020-02-23 13:01:34 +0100

    [improvement] Improved upper triangular matrix vector product algorithm

--- tests/upper_triangular_mvp_tests.py
+++ tests/upper_triangular_mvp_tests.py
@@ -1,14 +1,28 @@
-from upper_triangular_mvp import *
-from lib.benchmark import benchmark
+from ..lib.benchmark import benchmark
+from ..upper_triangular_mvp import *
 
 # Test data
-u = np.array([[1, 1, 1, 1], [0, 2, 2, 2], [0, 0, 3, 3], [0, 0, 0, 4]])
-x = np.array([1, 2, 3, 4])
-y = np.zeros(4, dtype=int)
+u = np.array([[1, 1, 1, 1, 1, 1], [0, 2, 2, 2, 2, 2], [0, 0, 3, 3, 3, 3], [0, 0, 0, 4, 4, 4], [0, 0, 0, 0, 5, 5], [0, 0, 0, 0, 0, 6]])
+x = np.array([1, 2, 3, 4, 5, 6])
+y = np.zeros(6, dtype=int)
+yexp = np.array([21, 40, 54, 60, 55, 36])
 
-# Benchmarks
+
+# Results -
+print('Dot product based - Result: %s. Expected: %s' % (mvp_var1(u, x, y), yexp))
+
+# Cleanup
+y = np.zeros(6, dtype=int)
+
+print('Axpy based - Result: %s. Expected: %s' % (mvp_var2(u, x, y), yexp))
+
+
+# Benchmarks -
 with benchmark('Dot product-based algorithm'):
     mvp_var1(u, x, y)
 
-with benchmark('Axpy-based algorithm'):
+# Cleanup
+y = np.zeros(6, dtype=int)
+
+with benchmark('uxpy-based algorithm'):
     mvp_var2(u, x, y)
--- upper_triangular_mvp.py
+++ upper_triangular_mvp.py
@@ -6,8 +6,7 @@ def mvp_var1(u, x, y):
     return y
 
 def mvp_var2(u, x, y):
-    y[0] = u[:, 0][0] * x[0] + y[0]
-    for k in range(1, len(u)):
+    for k in range(len(u)):
         y[:k] = u[:, k][:k] * x[k] + y[:k]
         y[k] = u[:, k][k] * x[k] + y[k]
     return y
